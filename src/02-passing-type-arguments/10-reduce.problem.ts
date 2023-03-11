b31b59a477783a3f8b3f80b81c106a8741f524e3
Sazzad Hossain <tanimsazzad@gmail.com>
2023-03-11 21:40:53 +0600

    Learned about Record<Key, Type>

--- src/02-passing-type-arguments/10-reduce.problem.ts
+++ src/02-passing-type-arguments/10-reduce.problem.ts
@@ -1,29 +1,29 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest'
+import { Equal, Expect } from '../helpers/type-utils'
 
 const array = [
-  {
-    name: "John",
-  },
-  {
-    name: "Steve",
-  },
-];
+	{
+		name: 'John',
+	},
+	{
+		name: 'Steve',
+	},
+]
 
-const obj = array.reduce((accum, item) => {
-  accum[item.name] = item;
-  return accum;
-}, {});
+const obj = array.reduce<Record<string, { name: string }>>((accum, item) => {
+	accum[item.name] = item
+	return accum
+}, {})
 
-it("Should resolve to an object where name is the key", () => {
-  expect(obj).toEqual({
-    John: {
-      name: "John",
-    },
-    Steve: {
-      name: "Steve",
-    },
-  });
+it('Should resolve to an object where name is the key', () => {
+	expect(obj).toEqual({
+		John: {
+			name: 'John',
+		},
+		Steve: {
+			name: 'Steve',
+		},
+	})
 
-  type tests = [Expect<Equal<typeof obj, Record<string, { name: string }>>>];
-});
+	type tests = [Expect<Equal<typeof obj, Record<string, { name: string }>>>]
+})
