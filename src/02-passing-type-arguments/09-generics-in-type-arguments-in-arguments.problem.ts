ab39e8170d5f43417fff555ff05eb70d56517090
Sazzad Hossain <tanimsazzad@gmail.com>
2023-03-11 20:27:27 +0600

    Update 09-generics-in-type-arguments-in-arguments.problem.ts
    Learned using generic in arrow function and using previous Generic type on other generic type

--- src/02-passing-type-arguments/09-generics-in-type-arguments-in-arguments.problem.ts
+++ src/02-passing-type-arguments/09-generics-in-type-arguments-in-arguments.problem.ts
@@ -1,30 +1,32 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest'
+import { Equal, Expect } from '../helpers/type-utils'
 
 export class Component<TProps> {
-  private props: TProps;
+	private props: TProps
 
-  constructor(props: TProps) {
-    this.props = props;
-  }
+	constructor(props: TProps) {
+		this.props = props
+	}
 
-  getProps = () => this.props;
+	getProps = () => this.props
+}
+// first <T> after equal sign represents A generic is going to be used.
+//  Which Type? the type that we got from Component, so we use Component<T>
+const cloneComponent = <T>(component: Component<T>) => {
+	return new Component(component.getProps())
 }
 
-const cloneComponent = (component: unknown) => {
-  return new Component(component.getProps());
-};
+it('Should clone the props from a passed-in Component', () => {
+	const component = new Component({ a: 1, b: 2, c: 3 })
+	const res = component.getProps()
 
-it("Should clone the props from a passed-in Component", () => {
-  const component = new Component({ a: 1, b: 2, c: 3 });
+	const clonedComponent = cloneComponent(component)
 
-  const clonedComponent = cloneComponent(component);
+	const result = clonedComponent.getProps()
 
-  const result = clonedComponent.getProps();
+	expect(result).toEqual({ a: 1, b: 2, c: 3 })
 
-  expect(result).toEqual({ a: 1, b: 2, c: 3 });
-
-  type tests = [
-    Expect<Equal<typeof result, { a: number; b: number; c: number }>>
-  ];
-});
+	type tests = [
+		Expect<Equal<typeof result, { a: number; b: number; c: number }>>
+	]
+})
