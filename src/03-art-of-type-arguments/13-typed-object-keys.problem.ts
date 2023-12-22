761dfa62f81a0ee4fd243239512d546febef29d8
Sazzad Hossain <tanimsazzad@gmail.com>
2023-12-22 12:31:28 +0600

    solved 13

--- src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
+++ src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
@@ -6,9 +6,9 @@ import { Equal, Expect } from "../helpers/type-utils";
  * to do with the way you specify the generic. Can you get
  * both solutions?
  */
-const typedObjectKeys = (obj: unknown) => {
-  return Object.keys(obj);
-};
+
+const typedObjectKeys = <T extends object>(obj: T) =>
+  Object.keys(obj) as Array<keyof T>;
 
 it("Should return the keys of the object", () => {
   const result1 = typedObjectKeys({
